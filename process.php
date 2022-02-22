e146c3c7f3249c4d2a205de7b301b3811f8cbe5f
wanter9091 <dhqudwo123@gmail.com>
2022-02-22 21:16:27 +0900

    db에 정보 전잘

--- process.php
+++ process.php
@@ -1,4 +1,7 @@
 <?php
+    $conn=mysqli_connect("localhost","root", "1q2w3e4r");
+	mysqli_select_db($conn,"opentutorials");
     $sql="INSERT INTO topic(title, description, author, created) VALUES('".$_POST['title']."', '".$_POST['description']."', '".$_POST['author']."', now())";
-    echo $sql;
+	$result=mysqli_query($conn,$sql);
+    header('Location: http://localhost/index.php');
 ?>
